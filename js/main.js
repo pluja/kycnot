9c1c5f6b47ce9bb5fe5b3a07b49c073c6a5a8273
Wire <github_pluja@r3d.red>
2020-06-12 17:40:03 +0200

    Add donation section

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ $('#legendModal').on('shown.bs.modal', function () {
     $('#legend').trigger('focus')
   })
 
+  function donateBtc() {
+    /* Get the text field */
+    var copyText = "1GafZ243XHrpteRJ4gNkm91xZHnmYUR96L"
+  
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
+
 (function ($) {
     "use strict";
 })(jQuery);
