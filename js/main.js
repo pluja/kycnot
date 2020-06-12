1678a1b4b0a7e32d32455a6e09c9fc067ca0b0b6
Wire <github_pluja@r3d.red>
2020-06-12 14:34:14 +0200

    Partially solve #5 and #6

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+$('#legendModal').on('shown.bs.modal', function () {
+    $('#legend').trigger('focus')
+  })
+
 (function ($) {
     "use strict";
 })(jQuery);
